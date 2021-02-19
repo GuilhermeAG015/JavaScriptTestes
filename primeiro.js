05b3b1335bc51c106b9d66ab92b80df1b86d105e
GuilhermeAG015 <61588741+GuilhermeAG015@users.noreply.github.com>
2021-02-19 09:43:48 -0300

    Adicionando o index.html e o primeiro.js

---
+++ primeiro.js
@@ -0,0 +1,54 @@
+function calculoSoma(n1, n2) {
+    var n3 = n1 + n2
+
+    return n3
+}
+
+function calculoSub(n1, n2) {
+    var n3 = n1 - n2
+
+    return n3
+}
+
+
+function calculoVezes(n1, n2) {
+    var n3 = n1 * n2
+
+    return n3
+}
+
+function calculoDiv(n1, n2) {
+    var n3 = n1 / n2
+
+    return n3
+}
+
+function calcula(n1, n2, i){
+    var n10 = n1
+    var n20 = n2
+    var j = i
+
+    if(j == 'soma'){
+        let n3 = calculoSoma(n10, n20)
+        console.log("O resultado da soma é: " + n3)
+    }
+    if(j == 'subtracacao'){
+        let n3 = calculoSub(n10, n20)
+        console.log("O resultado da subtracacao é: " + n3)
+    }
+    if(j == 'vezes'){
+        let n3 = calculoVezes(n10, n20)
+        console.log("O resultado da multiplicação é: " + n3)
+    }
+    if(j == 'divisao'){
+        let n3 = calculoDiv(n10, n20)
+        console.log("O resultado da divisão é: " + n3)
+    }
+}
+
+var n1 = 10
+var n2 = 2
+var i = 'soma'
+
+calcula(n1, n2, i)
+
