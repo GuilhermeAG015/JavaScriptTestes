47e2dc974ec7a04d1a55f08de84123afcfc83401
GuilhermeAG015 <61588741+GuilhermeAG015@users.noreply.github.com>
2021-02-24 22:38:01 -0300

    Adicionando um projeto em React

---
+++ ReactNextLevel/moveitnext/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello <b>Starkiller</b></h1>
+      <Button color="red"></Button><br />
+      <Button color="red"></Button><br />
+      <Button color="red"></Button><br />
+    </div>
+  );
+}
+
+export default App;
